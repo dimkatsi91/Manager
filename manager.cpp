6b33bc1723b451d431b753e7a8d129c0447fd79e
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2019-08-18 23:46:43 +0300

    Added show password in user mgmnt section

--- manager.cpp
+++ manager.cpp
@@ -45,6 +45,9 @@ void Manager::init()
     // Password should not be visible by default
     ui->password_lineEdit->setEchoMode(QLineEdit::Password);
     ui->retype_password_lineEdit->setEchoMode(QLineEdit::Password);
+    // Also the password for the new user who will be created should not
+    // be visible by default
+    ui->new_user_password_lineEdit->setEchoMode(QLineEdit::Password);
 }
 
 /* ============================================================================================================ */
